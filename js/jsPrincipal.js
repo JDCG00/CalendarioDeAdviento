c0a64ce20aa094d3b67f4f689619e08ca9957e70
JDCG <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2021-12-11 13:15:59 +0100

    v2

---
+++ js/jsPrincipal.js
@@ -0,0 +1,22 @@
+/** 
+    jsPrincipal.js
+    @file Controlador principal de la web de Navidad
+    @author Juan Diego Carretero Granado <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
+    @License GPL v3 2021
+*/
+
+'use strict'
+
+
+
+class Navidad{
+
+}
+class Vista{
+    
+}
+class Modelo{
+    
+}
+
+new Navidad()
