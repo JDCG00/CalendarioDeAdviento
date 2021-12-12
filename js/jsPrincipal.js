a5d3d6a572a46126e8ea525c3684955334bacc4c
JDCG <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2021-12-12 17:47:25 +0100

    v3

--- js/jsPrincipal.js
+++ js/jsPrincipal.js
@@ -12,26 +12,97 @@
 class Navidad{
 
     constructor(){        
-        this.Vista = new Vista()
-        this.Modelo = null
-        window.onload = this.iniciar.bind(this)
-        window.onclick = this.iniciar.bind(this)
+        this.vista = null
+        this.modelo = new Modelo()
+        window.onload = this.iniciar.bind(this)        
     }
     /**
        Inicia
     **/
     iniciar(){
-        console.log('ffsdsadas')
+        console.log('Cargado')
+        this.vista = new Vista()
+        window.onclick = this.finalizarAudio.bind(this)
     }
 
-    finalizarAudio() {
+    finalizarAudio() {        
         let audio = document.getElementById('audio')
-        audio.remove()
+        if (audio) 
+            audio.remove()   
     }
 }
 
 class Vista{
-    
+    constructor(){
+        this.particulas(this)
+    }
+    particulas(){
+        if (document.getElementById('detector')) {
+            let config_particulas = {
+                "composition":"source-over",
+                "count":400,
+                "speed":5,
+                "parallax":5,
+                "direction":160,
+                "xVariance":5,
+                "yVariance":0,
+                "rotate":true,
+                "rotation":5,
+                "alphaSpeed":2,
+                "alphaVariance":2,
+                "minAlpha":-1,
+                "maxAlpha":1,
+                "minSize":5,
+                "maxSize":12,
+                "style":"fill",
+                "bounce":false,
+                "drift":10,
+                "glow":0,
+                "twinkle":false,
+                "color":["white",
+                "#d3e8eb"],
+                "shape":["image",
+                "image",
+                "circle",
+                "image"],
+                "imageUrl":"./img/copo_nieve.png"
+            }    
+            let contenedor = document.querySelector(".particulas");
+            let misparticulas = new Sparticles(contenedor, config_particulas)
+        }else{
+            let config_particulas = {
+                "composition":"source-over",
+                "count":400,
+                "speed":5,
+                "parallax":5,
+                "direction":160,
+                "xVariance":5,
+                "yVariance":0,
+                "rotate":true,
+                "rotation":5,
+                "alphaSpeed":2,
+                "alphaVariance":2,
+                "minAlpha":-1,
+                "maxAlpha":1,
+                "minSize":5,
+                "maxSize":12,
+                "style":"fill",
+                "bounce":false,
+                "drift":10,
+                "glow":0,
+                "twinkle":false,
+                "color":["white",
+                "#d3e8eb"],
+                "shape":["image",
+                "image",
+                "circle",
+                "image"],
+                "imageUrl":"../img/copo_nieve.png"
+            }    
+            let contenedor = document.querySelector(".particulas");
+            let misparticulas = new Sparticles(contenedor, config_particulas)
+        }        
+    }
 }
 
 class Modelo{
