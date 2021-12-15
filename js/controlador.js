fbb615f9400b8644057dd7ea3046727c6eebfdf3
JDCG <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2021-12-15 23:58:52 +0100

    v3

--- js/controlador.js
+++ js/controlador.js
@@ -24,15 +24,30 @@ export class Navidad{
         console.log('Cargado')
         this.vista = new Vista()
         document.getElementById('stop').onclick = this.finalizarAudio.bind(this)
-        document.getElementById('play').onclick = this.vista.crearDiv.bind(this)
+        document.getElementById('play').onclick = this.crearAudio.bind(this)
+        document.getElementById('nieve').onclick = this.vista.quitarParticulas.bind(this)
         document.getElementsByClassName('contenedor-principal')[0].onclick = this.vista.verificar.bind(this.vista)
-        document.getElementsByTagName('body')[0].onclick = this.vista.testEvento.bind(this.vista)
-    }
-    
+        document.getElementsByTagName('body')[0].onclick = this.vista.testEvento.bind(this.vista)        
+        document.getElementsByClassName('pestañaCerrar')[0].onclick = this.vista.borrarTarjeta.bind(this.vista)               
+    } 
     finalizarAudio() {        
         let audio = document.getElementById('audio')
         if (audio) 
-            audio.remove()   
+            audio.remove()      
+    }
+    crearAudio(){
+        let audio = document.createElement('audio')
+        let body = document.getElementsByTagName('body')[0]
+        body.appendChild(audio)
+        function ponerAtributos(ele, atri) {
+            for (var i in atri) {
+                ele.setAttribute(i, atri[i])
+            }
+        }        
+        ponerAtributos(audio, {"id" : "audio", "autoplay" : "", "loop" : "" })
+        let source =  document.createElement('source')
+        source.setAttribute('src' , "./recursos/Jingle_Bells_Piano_Version.mp3") 
+        audio.appendChild(source)
     }
 }
 new Navidad()
--- js/vista.js
+++ js/vista.js
@@ -22,32 +22,27 @@ export class Vista{
         let cogerCoordY = this.modelo.getCoordY()
         for (let i = 0; i < 25; i++) {
             if (Math.abs(evento.layerX - cogerCoordX[i]) < 25 && Math.abs(evento.layerY - cogerCoordY[i]) < 25){
-                this.crearDiv()
+                this.verDiv()
                 this.crearTextoTarjeta(this.modelo.getTextoTarjeta()[i])
-                this.crearPestañaCerrar()
+                this.verPestañaCerrar()
             }                          
         }        
     }
 
-    crearDiv(){
-       let divTarjeta = document.createElement('div')              
+    verDiv(){
+       let divTarjeta = document.getElementsByClassName('tarjeta')[0]              
        divTarjeta.classList.add('tarjeta')
        function retraso(){
             divTarjeta.style.opacity = 1
             divTarjeta.style.transition = `opacity ${1}s`
+            divTarjeta.style.pointerEvents = "initial"
        }
        setTimeout(retraso, 100)
-       
-       let main = document.getElementsByTagName('main')[0]
-       main.appendChild(divTarjeta)
     }
-    crearPestañaCerrar(){
-        let divCerrar = document.createElement('div')
-        divCerrar.classList.add('pestañaCerrar')
-        let divTarjeta = document.getElementsByClassName('tarjeta')[0]
-        divTarjeta.appendChild(divCerrar)
-
-
+    verPestañaCerrar(){
+        let divCerrar = document.getElementsByClassName('pestañaCerrar')[0]
+        divCerrar.style.opacity = 1
+        divCerrar.style.pointerEvents = "initial"
     }
     crearTextoTarjeta(cogerTexto){
         let pTarjeta = document.createElement('p')
@@ -58,11 +53,90 @@ export class Vista{
 
         pTarjeta.appendChild(document.createTextNode(cogerTexto))
     }
+    borrarTarjeta(){
+        let tarjeta = document.getElementsByClassName('tarjeta')[0]
+        let pTarjeta = document.getElementsByClassName('pTarjeta')[0]
+        if(tarjeta){
+            tarjeta.style.opacity = 0            
+            tarjeta.style.transition = `opacity ${0.5}s`
+            tarjeta.style.pointerEvents = "none"
+            pTarjeta.remove()
+        }            
+    }
 
     quitarParticulas(){
-        let particulas = document.querySelector(".particulas")
-        if (particulas) {
-            particulas.remove()
+        let particulasCoger = document.querySelector(".particulas")
+        let particulasCrear = document.createElement('div')
+        particulasCrear.classList.add('particulas')
+        if (particulasCoger) {
+            particulasCoger.remove()
+        }else{
+            document.body.appendChild(particulasCrear)
+            if (document.getElementById('detector')) {
+                let config_particulas = {
+                    "composition":"source-over",
+                    "count":400,
+                    "speed":5,
+                    "parallax":5,
+                    "direction":160,
+                    "xVariance":5,
+                    "yVariance":0,
+                    "rotate":true,
+                    "rotation":5,
+                    "alphaSpeed":2,
+                    "alphaVariance":2,
+                    "minAlpha":-1,
+                    "maxAlpha":1,
+                    "minSize":5,
+                    "maxSize":12,
+                    "style":"fill",
+                    "bounce":false,
+                    "drift":10,
+                    "glow":0,
+                    "twinkle":false,
+                    "color":["white",
+                    "#d3e8eb"],
+                    "shape":["image",
+                    "image",
+                    "circle",
+                    "image"],
+                    "imageUrl":"./img/copo_nieve.png"
+                }    
+                let contenedor = document.querySelector(".particulas");
+                this.misparticulas = new Sparticles(contenedor, config_particulas)
+            }else{
+                let config_particulas = {
+                    "composition":"source-over",
+                    "count":400,
+                    "speed":5,
+                    "parallax":5,
+                    "direction":160,
+                    "xVariance":5,
+                    "yVariance":0,
+                    "rotate":true,
+                    "rotation":5,
+                    "alphaSpeed":2,
+                    "alphaVariance":2,
+                    "minAlpha":-1,
+                    "maxAlpha":1,
+                    "minSize":5,
+                    "maxSize":12,
+                    "style":"fill",
+                    "bounce":false,
+                    "drift":10,
+                    "glow":0,
+                    "twinkle":false,
+                    "color":["white",
+                    "#d3e8eb"],
+                    "shape":["image",
+                    "image",
+                    "circle",
+                    "image"],
+                    "imageUrl":"../img/copo_nieve.png"
+                }    
+                let contenedor = document.querySelector(".particulas");
+                this.misparticulas = new Sparticles(contenedor, config_particulas)
+            }        
         }
     }
     
@@ -98,7 +172,7 @@ export class Vista{
                 "imageUrl":"./img/copo_nieve.png"
             }    
             let contenedor = document.querySelector(".particulas");
-            let misparticulas = new Sparticles(contenedor, config_particulas)
+            this.misparticulas = new Sparticles(contenedor, config_particulas)
         }else{
             let config_particulas = {
                 "composition":"source-over",
@@ -130,7 +204,7 @@ export class Vista{
                 "imageUrl":"../img/copo_nieve.png"
             }    
             let contenedor = document.querySelector(".particulas");
-            let misparticulas = new Sparticles(contenedor, config_particulas)
+            this.misparticulas = new Sparticles(contenedor, config_particulas)
         }        
     }
 }
